c79aad14a4d2dd087b165079a91e49e8fec7dbd9
alsondab <alsondab9@gmail.com>
2024-07-19 16:49:29 +0000

    first commit

---
+++ rafistoback/app/Http/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'signup']]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Invalid Email or Password'], 401);
+        }
+        return $this->respondWithToken($token);
+    }
+
+    public function signup(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'prenom' => 'required',
+            'email' => 'required|email|unique:users',
+          
+            'password' => 'required|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'prenom' => $request->prenom,
+            'email' => $request->email,
+            'tel' => $request->tel,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return response()->json(['message' => 'User created successfully', 'user' => $user], 201);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+        ]);
+    }
+}
